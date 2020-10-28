c21d255a41ee1692bd6f570613106436cfd4ab06
dickm19 <maryclairedick@gmail.com>
2020-10-28 11:20:05 -0400

    refactored

--- index.js
+++ index.js
@@ -4,50 +4,29 @@ document.addEventListener("DOMContentLoaded", function() {
   
   let movesContainer = document.querySelector("#moves-container")
   let moves = [];
+
+  function addMove(dir){
+    moves.push(dir)
+    let li = document.createElement("li")
+      li.classList.add("move-list")
+      li.appendChild(document.createTextNode(dir))
+      movesContainer.appendChild(li)
+      li.addEventListener("click",function(){
+        li.remove()
+        delete moves[moves.indexOf(dir)]
+      })
+  }
   
   document.addEventListener("keydown",function(event){
     
     if (event.key == "ArrowLeft"){
-      moves.push("left")
-      let li = document.createElement("li")
-      li.classList.add("move-list")
-      li.appendChild(document.createTextNode("left"))
-      movesContainer.appendChild(li)
-      li.addEventListener("click",function(){
-        li.remove()
-        delete moves[moves.indexOf("left")]
-      })
+      addMove("left")
     } else if (event.key == "ArrowRight"){
-      moves.push("right")
-      let li = document.createElement("li")
-      li.appendChild(document.createTextNode("right"))
-      movesContainer.appendChild(li)
-      li.classList.add("move-list")
-      li.addEventListener("click",function(){
-        li.remove()
-        delete moves[moves.indexOf("right")]
-      })
+      addMove("right")
     } else if (event.key == "ArrowDown"){
-      moves.push("down")
-      let li = document.createElement("li")
-      li.appendChild(document.createTextNode("down"))
-      movesContainer.appendChild(li)
-      li.classList.add("move-list")
-
-      li.addEventListener("click",function(){
-        li.remove()
-        delete moves[moves.indexOf("down")]
-        })
+      addMove("down")
     } else if (event.key == "ArrowUp"){
-      moves.push("up")
-      let li = document.createElement("li")
-      li.appendChild(document.createTextNode("up"))
-      movesContainer.appendChild(li)
-      li.classList.add("move-list")
-      li.addEventListener("click",function(){
-        li.remove()
-        delete moves[moves.indexOf("up")]
-      })
+      addMove("up")
     }
   })
    
