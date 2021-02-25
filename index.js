2887755f4c6c5e2ab6c8a9a21544ab5feed3a528
gabriel-demes <Gabrieldemes@nyu.edu>
2021-02-25 10:40:59 -0500

    Halfway through

--- index.js
+++ index.js
@@ -3,4 +3,42 @@ document.addEventListener("DOMContentLoaded", function() {
   init();
 
   // ADD CODE HERE!
+  
+  // ***reference *****/
+  const movesContainer = document.querySelector("ul#moves-container");
+  
+
+  document.addEventListener ("keydown", function(event) { 
+    const movement = document.createElement('li');
+    console.log(event.code)
+    switch (event.code) {
+    
+        case "ArrowLeft": 
+          movement.innerHTML = "left";
+          movesContainer.appendChild(movement) 
+          break; 
+        case "ArrowUp": 
+        movement.innerHTML = "up";
+        movesContainer.appendChild(movement) 
+            break; 
+        case "ArrowRight": 
+        movement.innerHTML = "right";
+        movesContainer.appendChild(movement) 
+            break; 
+        case "ArrowDown": 
+        movement.innerHTML = "down";
+        movesContainer.appendChild(movement)  
+            break; 
+    } })
+
+    const moveButton = document.querySelector("button#move-button");
+
+    moveButton.addEventListener("click", function(){
+      move(movesContainer.children[0].innerHTML)
+      movesContainer.children[0].remove()
+    })
+
+    
+
+
 });
