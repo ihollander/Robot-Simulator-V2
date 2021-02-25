3c674ecffb3003e27b759cb50d8eb18a2413639f
celestialheathen <jingchaoyu@hotmail.com>
2021-02-25 11:04:56 -0500

    Done

--- index.js
+++ index.js
@@ -2,5 +2,63 @@ document.addEventListener("DOMContentLoaded", function() {
   // initializes the board
   init();
 
-  // ADD CODE HERE!
-});
+  const robot = document.querySelector('div.tile.robot');
+  const ul = document.querySelector('ul#moves-container')
+  const moveBtn = document.querySelector('button#move-button')
+
+  document.addEventListener('keydown', function(e) {
+      if (e.key === 'ArrowDown') { 
+        let li = document.createElement('li')
+        li.textContent = "Down"
+        ul.append(li)
+      }
+      if (e.key === 'ArrowUp') { 
+        let li = document.createElement('li')
+        li.textContent = "Up"
+        ul.append(li)
+      }
+      if (e.key === 'ArrowLeft') { 
+        let li = document.createElement('li')
+        li.textContent = "Left"
+        ul.append(li)
+      }
+      if (e.key === 'ArrowRight') { 
+        let li = document.createElement('li')
+        li.textContent = "Right"
+        ul.append(li)
+      }
+  })
+
+  // Working deliverable Move the Robot 
+
+  // moveBtn.addEventListener('click', function(e) {
+  //   const firstMove = ul.children[0]
+  //   move(firstMove.textContent.toLowerCase())
+  //   firstMove.remove()
+  // })
+
+  ul.addEventListener('click', function(e) {
+    if (e.target.tagName === "LI") {
+      e.target.remove()
+    }
+  })
+
+  
+  // Bonus
+
+  function moveRobot() { 
+    moveBtn.onclick = interval 
+    }
+    
+  function interval() {
+        setInterval(function() {        
+          const firstMove = ul.children[0]
+          move(firstMove.textContent.toLowerCase())
+          firstMove.remove()
+        }, 500)}
+
+  moveRobot();
+
+})
+
+
